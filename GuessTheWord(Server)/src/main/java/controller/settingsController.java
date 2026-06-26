5e7f2976046a37677d77ff015bf0effdcfbec042
PalermoEuplio <euppal123456789@gmail.com>
2026-06-26 13:45:48 +0200

    Terminato comportamento della parte sinistra della pagina impostazioni Partita del Server (Da implementare l'uso di Thread (Obbligatorio))

--- GuessTheWord(Server)/src/main/java/controller/settingsController.java
+++ GuessTheWord(Server)/src/main/java/controller/settingsController.java
@@ -6,8 +6,8 @@ package controller;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.stream.Collectors;
 import javafx.collections.FXCollections;
@@ -45,7 +45,7 @@ public class settingsController implements Initializable{
     private ListView<String> fileAnalizzati;
     
     @FXML
-    private TextArea risultatiAnalisi;
+    private TextArea risultatoAnalisi;
     
     @FXML
     private Slider frequenza;
@@ -66,14 +66,12 @@ public class settingsController implements Initializable{
     private TextField paroleScelte;
     
     
-    
+    private TextEditor te;
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        TextEditor te = new TextEditor();
         
-        areaTesto.appendText(te.caricaTesto("I_Promessi_Sposi"));
-        
+        te = new TextEditor();
         
         te.leggiReport();
         
@@ -81,44 +79,89 @@ public class settingsController implements Initializable{
         List<Testo> titleList = te.getTitle();
         
         // Preparo la lista dei file disponibili e se questi sono già stati analizzati
-        fileAnalizzati.setItems(FXCollections.observableArrayList(titleList.stream().map(
-                a -> {return (a.isAnalized()) ? a.getTitolo() + " (Analizzato)" : a.getTitolo(); }).collect(Collectors.toList())
-        
-        ));
+        aggiornaReportVideo();
         
         // Preparo il combo box per la selezione del testo da analizzare
         comboTesti.setItems(FXCollections.observableArrayList(titleList.stream().map(Testo::getTitolo).collect(Collectors.toList())));
 
         
-        
-        
+        // Preparo la parte dell'analisi del testo e carico il testo in base a quale file è stato selezionato nel comboBox
+        comboTesti.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            
+            if (newValue != null) {
+                
+                Testo testoSelezionato = te.getTitle().stream().filter(t -> t.getTitolo().equals(newValue)).findFirst().orElse(null);
+
+                if (testoSelezionato != null) {
+                    
+                    int id = testoSelezionato.getTxtId();
+                    // Carico il testo del relativo file nel textArea
+                    areaTesto.clear();  // Pulisco l'area di Testo
+                    System.out.println("TESTO:::::"+areaTesto.getText());
+                    areaTesto.setText(te.caricaTesto(testoSelezionato.getTitolo()));
+                    
+                    if (!testoSelezionato.isAnalized()) {
+                        
+                        risultatoAnalisi.setText("Il testo selezionato non è ancora stato analizzato.\nPremi il tasto 'Analizza' per generare le statistiche.");
+                        
+                    } else aggiornaAnalisiVideo(id);   // Aggiorno il contenuto del textArea
+                        
+                    
+                }
+            }
+        });
         
         
         
     }
     
     
+    // Metodo per l'aggiornamento grafico della lista contenente il report dei testi
+    private void aggiornaReportVideo() {
+        List<String> listaFormattata = te.getTitle().stream().map(
+                a -> a.isAnalized() ? a.getTitolo() + " (Analizzato)" : a.getTitolo()).collect(Collectors.toList());
+            
+        fileAnalizzati.setItems(FXCollections.observableArrayList(listaFormattata));
+    }
     
     
     
     
+    private void aggiornaAnalisiVideo(int id){
+        risultatoAnalisi.clear();   // Pulisco l'area dell'Analisi
+        
+        te.caricaAnalisi(id);
+        Map<String, Integer> mappaRisultati = te.getFrequency();
+        
+        if (mappaRisultati != null) {
+            formattazioneRisultatoanalisi(mappaRisultati);
+        } else { risultatoAnalisi.setText("Errore: Impossibile caricare l'analisi per questo file."); }
+        
+    }
     
     
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    private void formattazioneRisultatoanalisi(Map<String, Integer> mappa) {
+        
+        if (mappa.isEmpty()) {
+            risultatoAnalisi.setText("Il testo non contiene parole valide.");
+            return;
+        }
+        
+        int paroleTotali = mappa.values().stream().mapToInt(Integer::intValue).sum();   // Somma parole trovate
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Parole totali: ").append(paroleTotali).append("\n");
+        sb.append("Frequenza:\n");
+
+        // Ordinamento in ordine crescente
+        mappa.entrySet().stream()
+                .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue())) 
+                .forEach(e -> sb.append("- ").append(e.getKey()).append(": ").append(e.getValue()).append("\n"));
+
+        risultatoAnalisi.setText(sb.toString());    // Collegamento alla textArea
+    }
     
     
     
@@ -137,7 +180,17 @@ public class settingsController implements Initializable{
     
     @FXML
     private void analisiTesto() throws IOException {
-          
+        
+        String titolo = comboTesti.getValue();
+        Testo t = te.getTitle().stream().filter(t0 -> t0.getTitolo().equals(titolo)).findFirst().orElse(null);
+        
+        if(titolo!=null && t.isAnalized()==false){
+            
+            te.analizzaTesto(t.getTxtId());    // Analizzo il testo e aggiorno la lista
+            aggiornaReportVideo();  // Aggiorno la lista a video
+            aggiornaAnalisiVideo(t.getTxtId()); // Aggiorno l'area d'analisi a video
+            
+        }
     }
     
     @FXML
--- GuessTheWord(Server)/src/main/java/model/TextEditor.java
+++ GuessTheWord(Server)/src/main/java/model/TextEditor.java
@@ -4,12 +4,16 @@
  */
 package model;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -18,6 +22,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  *
@@ -56,7 +61,7 @@ public class TextEditor {
     
     // Metodi di gestione Testo
     
-    
+    // Metodo per caricare tutto il testo nella stringa selectedText
     public String caricaTesto(String fileName){
         
         File file = new File("../testi/"+fileName+".txt");
@@ -67,9 +72,11 @@ public class TextEditor {
         
         // Forzo la lettura con UTF_8
         try (BufferedReader br = Files.newBufferedReader(file.toPath(), StandardCharsets.UTF_8)){
+            selectedText = "";  // Cancello il testo precedente
+            
             String line;
             while((line = br.readLine())!=null){
-                selectedText = selectedText.concat(" "+line);
+                selectedText = selectedText.concat(" "+line);   // Leggo il testo riga per riga
             }
                 
             
@@ -81,7 +88,7 @@ public class TextEditor {
         return  selectedText;
     }
     
-    
+    // Metodo per leggere il Report e caricare i dati nella lista title
     public void leggiReport() {
         
         File fileCsv = new File("../analisiTesti/report.csv"); 
@@ -112,11 +119,20 @@ public class TextEditor {
         } catch (Exception e) { System.err.println("Errore nel caricamento del report: "+e); }
     }
     
-    
+    // Metodo per il solo caricamento dell'analisi del testo specificato
     public void caricaAnalisi(Integer txtId){
         
+        String filename = "../analisiTesti/"+title.stream().filter(a -> a.getTxtId() == txtId).map(a -> a.getTitolo()).findFirst().orElse("Testo Sconosciuto") + "-Analisi.dat";
+        try (ObjectInputStream ob = new ObjectInputStream(
+                new BufferedInputStream(
+                        new FileInputStream(filename)))){
+            
+            frequency = (HashMap<String, Integer>)ob.readObject();
+            ob.close();
+        } catch (Exception e) {System.out.println("Errore durante la lettura dell'analisi: "+e);}
     }
     
+    // Metodo per effettuare l'analisi del testo selezionato
     public boolean analizzaTesto(Integer txtId){
         
         // Salvo il nome che il file d'analisi dovrà avere
@@ -157,14 +173,44 @@ public class TextEditor {
             
             ob.writeObject(frequency);
             
+            ob.close();
         } catch (Exception e) { System.err.println("Errore durante l'analisi: "+e); return false;}
         
         aggiornaReport(txtId);
         return true;
     }
     
-    
+    // Metodo per aggiornare il file report con la nuova analisi effettuata (Aggiorna anche il valore isAnalized del testo nella lista title)
     public void aggiornaReport(Integer txtId){
+        
+        title.stream().filter(t -> t.getTxtId()==txtId).forEach(t -> t.setIsAnalized(true));
+        
+        File fileCsv = new File("../analisiTesti/report.csv");
+        
+       if (!fileCsv.exists()) {
+            System.err.println("File CSV non trovato!");
+            return;
+        }
+        
+        try (BufferedWriter br = Files.newBufferedWriter(fileCsv.toPath())) {
+            
+            br.write("Id");
+            br.write(";");
+            br.write("Titolo");
+            br.write(";");
+            br.write("Stato");
+            br.write("\n");
+            
+            for(Testo x : title){
+                br.write(String.valueOf(x.getTxtId()));
+                br.write(";");
+                br.write(x.getTitolo());
+                br.write(";");
+                br.write(String.valueOf((x.isAnalized()) ? 1 : 0));
+                br.write("\n");
+            }
+            br.close();
+        } catch (Exception e) { System.err.println("Errore nella scrittura del report: "+e); }
         
     }
     /*
